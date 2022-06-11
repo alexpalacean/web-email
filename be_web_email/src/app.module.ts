95380360dbf18e79abab4e551c45be63bb99e63b
Alex Palacean <alexpalacean@yahoo.com>
2022-06-11 22:32:12 +0300

    added database

--- be_web_email/src/app.module.ts
+++ be_web_email/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+
+import { Connection } from 'mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  @InjectConnection() private connection: Connection;
+
+  onModuleInit() {
+    if(this.connection){
+      console.log('🚀 database connected');
+    }
+  }
+}
